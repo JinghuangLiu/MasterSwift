1dd6d7f9118d3cda8cecb01dfc3e44fc8a65f9cd
刘靖煌 <liujinghuang@icloud.com>
2021-04-15 11:08:02 +0800

    新增设置显示汇编例子

--- 001、SwiftcDemo/SwiftcDemo/main.swift
+++ 001、SwiftcDemo/SwiftcDemo/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
+var a = 10
+var b = 20
+var c = a + b
+
 print("Hello, World!")
 
---
+++ 002、DisassemblyDemo/002、DisassemblyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  002、DisassemblyDemo
+//
+//  Created by 刘靖煌 on 2021/4/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int c = sizeof(int);
+        printf("%d",c);
+    }
+    return 0;
+}
