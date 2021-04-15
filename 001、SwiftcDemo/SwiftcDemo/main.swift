e4f4dba404744eb2ea718ace680fdd157db8e650
刘靖煌 <liujinghuang@icloud.com>
2021-04-15 13:59:52 +0800

    补充Swift语法的playground

--- 001、SwiftcDemo/SwiftcDemo/main.swift
+++ 001、SwiftcDemo/SwiftcDemo/main.swift
@@ -7,9 +7,5 @@
 
 import Foundation
 
-var a = 10
-var b = 20
-var c = a + b
-
 print("Hello, World!")
 
---
+++ 003、SwiftSyntax.playground/Contents.swift
@@ -0,0 +1,14 @@
+/// 不需要写main函数
+print("Hello, World!")
+
+/// 变量
+var a = 10
+a = 20
+
+/// 常量：赋值后不能改变
+let b = 30
+//b = 40 （Cannot assign to value: 'b' is a 'let' constant）
+
+/// 打印
+print(a)
+print("插入一个值：\(b)")
