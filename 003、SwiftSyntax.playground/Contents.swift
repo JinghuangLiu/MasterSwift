aa351e5224aaadd9065ac391e9f193e703bf49e5
刘靖煌 <liujinghuang@icloud.com>
2021-04-15 14:11:37 +0800

    playground 里添加视图

--- 003、SwiftSyntax.playground/Contents.swift
+++ 003、SwiftSyntax.playground/Contents.swift
@@ -12,3 +12,20 @@ let b = 30
 /// 打印
 print(a)
 print("插入一个值：\(b)")
+
+import UIKit
+import PlaygroundSupport
+
+//let view = UIView()
+//view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//view.backgroundColor = UIColor.red
+//PlaygroundPage.current.liveView = view
+
+let imageView = UIImageView(image: UIImage(named: "IMG_5708.JPG"))
+imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+PlaygroundPage.current.liveView = imageView
+
+let vc = UITableViewController()
+vc.view.backgroundColor = .lightGray
+PlaygroundPage.current.liveView = vc
+
